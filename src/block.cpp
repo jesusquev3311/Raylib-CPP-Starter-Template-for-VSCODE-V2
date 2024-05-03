bb01bd9169af3ada5f065afc537b622e91fd8d51
jesusquev3311 <jesusquev3311@gmail.com>
2024-05-03 23:04:23 +0200

    adding block class and refactoring colors

---
+++ src/block.cpp
@@ -0,0 +1,13 @@
+#include "block.h"
+
+Block::Block()
+{
+    cellSize = 30;
+    rotationState = 0;
+    colors = GetCellColors();
+}
+
+void Block::Draw()
+{
+    
+}
--- src/grid.cpp
+++ src/grid.cpp
@@ -1,4 +1,5 @@
 #include "grid.h"
+#include "colors.h"
 #include <iostream>
 
 Grid::Grid()
@@ -32,22 +33,6 @@ void Grid::Print()
         }
         std::cout << std::endl;
     }
-}
-
-std::vector<Color> Grid::GetCellColors()
-{
-    const Color darkGrey = {26, 31, 40, 255};
-    const Color green = {47, 230, 23, 255};
-    const Color red = {232, 18, 18, 255};
-    const Color orange = {226, 116, 17, 255};
-    const Color yellow = {237, 234, 4, 255};
-    const Color purple = {166, 0, 247, 255};
-    const Color cyan = {21, 204, 209, 255};
-    const Color blue = {13, 64, 216, 255};
-    const Color lightBlue = {59, 85, 162, 255};
-    const Color darkBlue = {44, 44, 127, 255};
-
-    return {darkGrey, green, red, orange, yellow, purple, cyan, blue, lightBlue, darkBlue};
 }
 
 void Grid::Draw()
