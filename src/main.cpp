003f1590e009aa7e0d52d3f93ba334ee2f89618b
jesusquev3311 <jesusquev3311@gmail.com>
2024-05-04 21:34:56 +0200

    feat: Game class, refactoring code and adding getRandomBlock and GetAllBlocks methods

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,18 @@
 #include <raylib.h>
-#include "grid.h"
-#include "blocks.cpp"
+#include "game.h"
 
 int main()
 {
     InitWindow(300, 600, "Tetris");
     SetTargetFPS(60);
 
-    Grid grid = Grid();
-    grid.Print();
-
-    OBlock block = OBlock();
+    Game game = Game();
 
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
         ClearBackground({44,44,125,255});
-        grid.Draw();
-        block.Draw();
+        game.Draw();
         EndDrawing();
     }
 
